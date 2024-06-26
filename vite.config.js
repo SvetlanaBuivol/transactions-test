2d751afb40dce1351d73b7aea456643fa5bb9abd
SvetlanaBuuvol <divinesvetok@gmail.com>
2024-06-26 22:51:45 +0300

    homepage, index.html, 404.html

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/transactions-test/',
   plugins: [react()],
+  base: "/transactions-test/",
 })
