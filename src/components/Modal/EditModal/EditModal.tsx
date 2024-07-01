5eefd4c39254b9235181a6329469f5d69d2045ca
SvetlanaBuuvol <divinesvetok@gmail.com>
2024-07-01 13:44:26 +0300

    delete comments

--- src/components/Modal/EditModal/EditModal.tsx
+++ src/components/Modal/EditModal/EditModal.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useState } from "react";
+import { FC, useEffect } from "react";
 import { Button } from "@mui/material";
 import Modal from "../Modal";
 import {
@@ -44,103 +44,47 @@ const EditModal: FC<EditModalProps> = ({
 
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
-        <Title>Edit Transaction Type</Title>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <CheckboxWrapper>
-            <Controller
-              name="newType"
-              control={control}
-              render={({ field }) => (
-                <>
-                  <StyledLabel>
-                    <input
-                      type="radio"
-                      value="Refill"
-                      checked={field.value === "Refill"}
-                      onChange={() => field.onChange("Refill")}
-                    />
-                    Refill
-                  </StyledLabel>
-                  <StyledLabel>
-                    <input
-                      type="radio"
-                      value="Withdrawal"
-                      checked={field.value === "Withdrawal"}
-                      onChange={() => field.onChange("Withdrawal")}
-                    />
-                    Withdrawal
-                  </StyledLabel>
-                </>
-              )}
-            />
-          </CheckboxWrapper>
-          <ButtonWrapper>
-            <Button size="small" variant="contained" type="submit">
-              Update
-            </Button>
-            <Button size="small" variant="contained" onClick={onCancel}>
-              Cancel
-            </Button>
-          </ButtonWrapper>
-        </form>
+      <Title>Edit Transaction Type</Title>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <CheckboxWrapper>
+          <Controller
+            name="newType"
+            control={control}
+            render={({ field }) => (
+              <>
+                <StyledLabel>
+                  <input
+                    type="radio"
+                    value="Refill"
+                    checked={field.value === "Refill"}
+                    onChange={() => field.onChange("Refill")}
+                  />
+                  Refill
+                </StyledLabel>
+                <StyledLabel>
+                  <input
+                    type="radio"
+                    value="Withdrawal"
+                    checked={field.value === "Withdrawal"}
+                    onChange={() => field.onChange("Withdrawal")}
+                  />
+                  Withdrawal
+                </StyledLabel>
+              </>
+            )}
+          />
+        </CheckboxWrapper>
+        <ButtonWrapper>
+          <Button size="small" variant="contained" type="submit">
+            Update
+          </Button>
+          <Button size="small" variant="contained" onClick={onCancel}>
+            Cancel
+          </Button>
+        </ButtonWrapper>
+      </form>
     </Modal>
   );
 };
-
-// const EditModal: FC<EditModalProps> = ({
-//   isOpen,
-//   onClose,
-//   transaction,
-//   onUpdateType,
-//   onCancel,
-// }) => {
-//   const [newType, setNewType] = useState<string>("");
-
-//   useEffect(() => {
-//     if (transaction && transaction.Type) {
-//       setNewType(transaction.Type);
-//     } else {
-//       setNewType("");
-//     }
-//   }, [transaction]);
-
-//   const handleUpdateType = () => {
-//     onUpdateType(newType);
-//     onClose();
-//   };
-//   return (
-//     <Modal isOpen={isOpen} onClose={onClose}>
-//       <Title>Edit Transaction Type</Title>
-//       <CheckboxWrapper>
-//         <StyledLabel>
-//           <input
-//             type="radio"
-//             value="refill"
-//             checked={newType === "Refill"}
-//             onChange={() => setNewType("Refill")}
-//           />
-//           Refill
-//         </StyledLabel>
-//         <StyledLabel>
-//           <input
-//             type="radio"
-//             value="withdrawal"
-//             checked={newType === "Withdrawal"}
-//             onChange={() => setNewType("Withdrawal")}
-//           />
-//           Withdrawal
-//         </StyledLabel>
-//       </CheckboxWrapper>
-//       <ButtonWrapper>
-//         <Button size="small" variant="contained" onClick={handleUpdateType}>
-//           Update
-//         </Button>
-//         <Button size="small" variant="contained" onClick={onCancel}>
-//           Cansel
-//         </Button>
-//       </ButtonWrapper>
-//     </Modal>
-//   );
-// };
 
 export default EditModal;
